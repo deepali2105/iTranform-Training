8fc2f2207d5bb3164daad59eb9960e505796f982
deepali2105 <78631237+deepali2105@users.noreply.github.com>
2021-03-22 13:59:35 +0530

    Update Collection2.java

--- collection/Collection2.java
+++ collection/Collection2.java
@@ -1,1 +1,20 @@
+package collections;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class Collection2 {
+   public static void main(String args[]) {
+      LinkedHashSet<String> linkedHS = new LinkedHashSet();
+      linkedHS.add("Apple");
+      linkedHS.add("B");
+      linkedHS.add("C");
+      linkedHS.add("D");
+      linkedHS.add("E");
+      Iterator<String> iterator = linkedHS.iterator();
+      while (iterator.hasNext()) {
+         System.out.println(iterator.next());
+      }
+   }
+}
 
